19355133cd0a4e8bc750036e84ed8518b7eba5a8
B. Zschiedrich <zschiedrich@systopia.de>
2019-10-22 15:37:16 +0200

    Removing of log out for new Civi menu (v.5.12+)

--- js/removeLogOut.js
+++ js/removeLogOut.js
@@ -4,12 +4,29 @@
         "DOMContentLoaded",
         function()
         {
-            // Disable all logout links:
-            const elements = document.getElementsByClassName('crm-logout-link');
-            for (const element of elements)
-            {
-                element.style.display = 'none';
-            }
+            // Old menu:
+            disableLogoutClassElements();
+            // New menu (Civi 5.12+):
+            disableLogoutListElements();
         }
     );
+
+    function disableLogoutClassElements ()
+    {
+        const elements = document.getElementsByClassName('crm-logout-link');
+        for (const element of elements)
+        {
+            element.style.display = 'none';
+        }
+    }
+
+    function disableLogoutListElements ()
+    {
+        const civiMenu = document.getElementById('civicrm-menu');
+        const elements = civiMenu.querySelectorAll('[data-name="Log out"]');
+        for (const element of elements)
+        {
+            element.style.display = 'none';
+        }
+    }
 })();
